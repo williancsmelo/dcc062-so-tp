4805400dd90565303d086dc18970dab8bd9d23b9
williancsmelo <melo.williancs@gmail.com>
2023-12-07 18:04:44 -0300

    created myfs format

--- myfs.c
+++ myfs.c
@@ -1,12 +1,12 @@
 /*
-*  myfs.c - Implementacao do sistema de arquivos MyFS
-*
-*  Autores: SUPER_PROGRAMADORES_C
-*  Projeto: Trabalho Pratico II - Sistemas Operacionais
-*  Organizacao: Universidade Federal de Juiz de Fora
-*  Departamento: Dep. Ciencia da Computacao
-*
-*/
+ *  myfs.c - Implementacao do sistema de arquivos MyFS
+ *
+ *  Autores: SUPER_PROGRAMADORES_C
+ *  Projeto: Trabalho Pratico II - Sistemas Operacionais
+ *  Organizacao: Universidade Federal de Juiz de Fora
+ *  Departamento: Dep. Ciencia da Computacao
+ *
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,102 +15,130 @@
 #include "inode.h"
 #include "util.h"
 
-//Declaracoes globais
-//...
-//...
+// Declaracoes globais
+#define MAX_FILES 100
 
-
-//Funcao para verificacao se o sistema de arquivos está ocioso, ou seja,
-//se nao ha quisquer descritores de arquivos em uso atualmente. Retorna
-//um positivo se ocioso ou, caso contrario, 0.
-int myFSIsIdle (Disk *d) {
+// Funcao para verificacao se o sistema de arquivos está ocioso, ou seja,
+// se nao ha quisquer descritores de arquivos em uso atualmente. Retorna
+// um positivo se ocioso ou, caso contrario, 0.
+int myFSIsIdle(Disk *d)
+{
 	return 0;
 }
 
-//Funcao para formatacao de um disco com o novo sistema de arquivos
-//com tamanho de blocos igual a blockSize. Retorna o numero total de
-//blocos disponiveis no disco, se formatado com sucesso. Caso contrario,
-//retorna -1.
-int myFSFormat (Disk *d, unsigned int blockSize) {
+// Funcao para formatacao de um disco com o novo sistema de arquivos
+// com tamanho de blocos igual a blockSize. Retorna o numero total de
+// blocos disponiveis no disco, se formatado com sucesso. Caso contrario,
+// retorna -1.
+int myFSFormat(Disk *d, unsigned int blockSize /* tamanho do cluster */)
+{
+	// Verificação de parâmetros inválidos
+	if (d == NULL || blockSize <= 0)
+		return -1;
+
+	unsigned long totalBlocks = diskGetSize(d) / blockSize;
+
+	// Inicializar i-nodes
+	for (int i = 0; i < MAX_FILES; i++)
+	{
+		if (inodeCreate(i, d) == NULL)
+			return -1;
+	}
+
+	return totalBlocks;
+}
+
+// Funcao para abertura de um arquivo, a partir do caminho especificado
+// em path, no disco montado especificado em d, no modo Read/Write,
+// criando o arquivo se nao existir. Retorna um descritor de arquivo,
+// em caso de sucesso. Retorna -1, caso contrario.
+int myFSOpen(Disk *d, const char *path)
+{
 	return -1;
 }
 
-//Funcao para abertura de um arquivo, a partir do caminho especificado
-//em path, no disco montado especificado em d, no modo Read/Write,
-//criando o arquivo se nao existir. Retorna um descritor de arquivo,
-//em caso de sucesso. Retorna -1, caso contrario.
-int myFSOpen (Disk *d, const char *path) {
-	return -1;
-}
-	
-//Funcao para a leitura de um arquivo, a partir de um descritor de
-//arquivo existente. Os dados lidos sao copiados para buf e terao
-//tamanho maximo de nbytes. Retorna o numero de bytes efetivamente
-//lidos em caso de sucesso ou -1, caso contrario.
-int myFSRead (int fd, char *buf, unsigned int nbytes) {
+// Funcao para a leitura de um arquivo, a partir de um descritor de
+// arquivo existente. Os dados lidos sao copiados para buf e terao
+// tamanho maximo de nbytes. Retorna o numero de bytes efetivamente
+// lidos em caso de sucesso ou -1, caso contrario.
+int myFSRead(int fd, char *buf, unsigned int nbytes)
+{
 	return -1;
 }
 
-//Funcao para a escrita de um arquivo, a partir de um descritor de
-//arquivo existente. Os dados de buf serao copiados para o disco e
-//terao tamanho maximo de nbytes. Retorna o numero de bytes
-//efetivamente escritos em caso de sucesso ou -1, caso contrario
-int myFSWrite (int fd, const char *buf, unsigned int nbytes) {
+// Funcao para a escrita de um arquivo, a partir de um descritor de
+// arquivo existente. Os dados de buf serao copiados para o disco e
+// terao tamanho maximo de nbytes. Retorna o numero de bytes
+// efetivamente escritos em caso de sucesso ou -1, caso contrario
+int myFSWrite(int fd, const char *buf, unsigned int nbytes)
+{
 	return -1;
 }
 
-//Funcao para fechar um arquivo, a partir de um descritor de arquivo
-//existente. Retorna 0 caso bem sucedido, ou -1 caso contrario
-int myFSClose (int fd) {
+// Funcao para fechar um arquivo, a partir de um descritor de arquivo
+// existente. Retorna 0 caso bem sucedido, ou -1 caso contrario
+int myFSClose(int fd)
+{
 	return -1;
 }
 
-//Funcao para abertura de um diretorio, a partir do caminho
-//especificado em path, no disco indicado por d, no modo Read/Write,
-//criando o diretorio se nao existir. Retorna um descritor de arquivo,
-//em caso de sucesso. Retorna -1, caso contrario.
-int myFSOpenDir (Disk *d, const char *path) {
+// Funcao para abertura de um diretorio, a partir do caminho
+// especificado em path, no disco indicado por d, no modo Read/Write,
+// criando o diretorio se nao existir. Retorna um descritor de arquivo,
+// em caso de sucesso. Retorna -1, caso contrario.
+int myFSOpenDir(Disk *d, const char *path)
+{
+	// skip
 	return -1;
 }
 
-//Funcao para a leitura de um diretorio, identificado por um descritor
-//de arquivo existente. Os dados lidos correspondem a uma entrada de
-//diretorio na posicao atual do cursor no diretorio. O nome da entrada
-//e' copiado para filename, como uma string terminada em \0 (max 255+1).
-//O numero do inode correspondente 'a entrada e' copiado para inumber.
-//Retorna 1 se uma entrada foi lida, 0 se fim de diretorio ou -1 caso
-//mal sucedido
-int myFSReadDir (int fd, char *filename, unsigned int *inumber) {
+// Funcao para a leitura de um diretorio, identificado por um descritor
+// de arquivo existente. Os dados lidos correspondem a uma entrada de
+// diretorio na posicao atual do cursor no diretorio. O nome da entrada
+// e' copiado para filename, como uma string terminada em \0 (max 255+1).
+// O numero do inode correspondente 'a entrada e' copiado para inumber.
+// Retorna 1 se uma entrada foi lida, 0 se fim de diretorio ou -1 caso
+// mal sucedido
+int myFSReadDir(int fd, char *filename, unsigned int *inumber)
+{
+	// skip
 	return -1;
 }
 
-//Funcao para adicionar uma entrada a um diretorio, identificado por um
-//descritor de arquivo existente. A nova entrada tera' o nome indicado
-//por filename e apontara' para o numero de i-node indicado por inumber.
-//Retorna 0 caso bem sucedido, ou -1 caso contrario.
-int myFSLink (int fd, const char *filename, unsigned int inumber) {
+// Funcao para adicionar uma entrada a um diretorio, identificado por um
+// descritor de arquivo existente. A nova entrada tera' o nome indicado
+// por filename e apontara' para o numero de i-node indicado por inumber.
+// Retorna 0 caso bem sucedido, ou -1 caso contrario.
+int myFSLink(int fd, const char *filename, unsigned int inumber)
+{
+	// skip
 	return -1;
 }
 
-//Funcao para remover uma entrada existente em um diretorio, 
-//identificado por um descritor de arquivo existente. A entrada e'
-//identificada pelo nome indicado em filename. Retorna 0 caso bem
-//sucedido, ou -1 caso contrario.
-int myFSUnlink (int fd, const char *filename) {
+// Funcao para remover uma entrada existente em um diretorio,
+// identificado por um descritor de arquivo existente. A entrada e'
+// identificada pelo nome indicado em filename. Retorna 0 caso bem
+// sucedido, ou -1 caso contrario.
+int myFSUnlink(int fd, const char *filename)
+{
+	// skip
 	return -1;
 }
 
-//Funcao para fechar um diretorio, identificado por um descritor de
-//arquivo existente. Retorna 0 caso bem sucedido, ou -1 caso contrario.	
-int myFSCloseDir (int fd) {
+// Funcao para fechar um diretorio, identificado por um descritor de
+// arquivo existente. Retorna 0 caso bem sucedido, ou -1 caso contrario.
+int myFSCloseDir(int fd)
+{
+	// skip
 	return -1;
 }
 
-//Funcao para instalar seu sistema de arquivos no S.O., registrando-o junto
-//ao virtual FS (vfs). Retorna um identificador unico (slot), caso
-//o sistema de arquivos tenha sido registrado com sucesso.
-//Caso contrario, retorna -1
-int installMyFS (void) {
+// Funcao para instalar seu sistema de arquivos no S.O., registrando-o junto
+// ao virtual FS (vfs). Retorna um identificador unico (slot), caso
+// o sistema de arquivos tenha sido registrado com sucesso.
+// Caso contrario, retorna -1
+int installMyFS(void)
+{
 	FSInfo *myfs = (FSInfo *)malloc(sizeof(FSInfo));
 	myfs->fsid = 'A';
 	myfs->fsname = "WillianFS";
@@ -126,7 +154,7 @@ int installMyFS (void) {
 	myfs->unlinkFn = myFSUnlink;
 	myfs->closedirFn = myFSCloseDir;
 
- 	int result = vfsRegisterFS(myfs);
+	int result = vfsRegisterFS(myfs);
 	// free(myfs);
 	return result;
 }
